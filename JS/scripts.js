7a59e288bdb5fe9ba7fe885cb8de7de9054e187c
Ahmedn-Essam <ahmed.essam.hassan12@gmail.com>
2024-05-19 16:54:33 +0300

    Add files via upload

---
+++ JS/scripts.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.security-card').forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.style.transform = 'translateY(-10px)';
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = 'translateY(0)';
+    });
+});
